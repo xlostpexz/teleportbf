bc96cd3d9beb192b7f99b56edf50afb6f8b49682
xlostpexz <98389349+xlostpexz@users.noreply.github.com>
2022-10-10 18:00:00 +0700

    Update and rename README.md to teleportbf.lua

---
+++ teleportbf.lua
@@ -0,0 +1,213 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("XzE Hub - Teleport", "Synapse")
+
+-- Teleport BF W2
+
+local Tab = Window:NewTab("Teleport Island (W1)")
+local Section = Tab:NewSection("Teleport BF V.3BB KAKKAK")
+Section:NewButton("Teleport Jungle", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1609.94031, 36.8520851, 150.758667, 0.81433624, 4.97434129e-08, 0.580393374, -1.14408989e-08, 1, -6.96539217e-08, -0.580393374, 5.00814892e-08, 0.81433624)
+    wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Prison", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(4874.05273, 5.65191603, 734.412109, 0.0128908567, 3.68098299e-08, -0.999916911, -7.29335952e-08, 1, 3.58726346e-08, 0.999916911, 7.24651059e-08, 0.0128908567)
+     wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Middle Town", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-650.985229, 7.85222101, 1571.57654, 0.256446242, 5.89117803e-08, -0.966558516, -1.81054838e-09, 1, 6.04696737e-08, 0.966558516, -1.37572185e-08, 0.256446242)
+     wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Magma", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5328.8335, 8.59067345, 8444.20703, -0.839769721, -3.87916153e-08, 0.542942762, 1.01855386e-08, 1, 8.72009451e-08, -0.542942762, 7.87588732e-08, -0.839769721)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Desert", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(984.823364, 6.56958914, 4249.4834, 0.818499744, 8.49807407e-08, 0.574506938, -6.42621814e-08, 1, -5.63651383e-08, -0.574506938, 9.21578014e-09, 0.818499744)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Admiral", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-4884.01953, 20.6520329, 4025.76904, 0.219530851, 1.61173368e-08, -0.975605547, 7.38082164e-08, 1, 3.31286714e-08, 0.975605547, -7.92804755e-08, 0.219530851)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Mob Leader", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2843.89136, 7.39224672, 5329.70166, 0.975887716, -5.53693136e-09, -0.218273148, 5.25076249e-09, 1, -1.89110327e-09, 0.218273148, 6.9940409e-10, 0.975887716)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Pirate", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1201.97522, 63.9375763, 3811.42896, 0.431913346, 0, -0.901915133, -0, 1.00000012, -0, 0.901915133, 0, 0.431913346)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Strater (Pirate)", "3BB KAKKAK", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1102.60156, 16.2701969, 1446.23584, -0.391487002, -0, -0.920183659, -0, 1, -0, 0.920183659, 0, -0.391487002)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Strater (Marine)", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2539.25684, 6.85559702, 2042.26782, 0.169187441, 0, -0.985583961, -0, 1.00000012, -0, 0.985583961, 0, 0.169187441)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Water 7", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5204.58887, 38.5011292, 4117.97852, 0.568745196, 0, 0.822513759, -0, 1, -0, -0.822513759, 0, 0.568745196)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport FishMan", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3875.94531, 16.4864597, -1960.50903, 0.665225863, 0, -0.746642172, -0, 1, -0, 0.746642172, 0, 0.665225863)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Colosseum", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1389.02307, 7.28907251, -2913.23413, -0.995376289, -6.39922337e-09, 0.0960520729, -6.86997392e-09, 1, -4.57029126e-09, -0.0960520729, -5.20903498e-09, -0.995376289)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Sky Pia 1", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-4888.81201, 717.68634, -2626.64844, 0.576539397, 0, 0.817069352, -0, 1, -0, -0.817069471, 0, 0.576539338)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Sky Pia 2", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-7880.86865, 5545.57715, -391.195831, -0.728085339, -0, -0.685486555, -0, 1.00000012, -0, 0.685486555, 0, -0.728085339)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Frozen Town", "3BB KAKKAK", function()
+     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1133.30859, 26.6127472, -1186.96423, -0.920246303, 0, 0.39133966, 0, 1, -0, -0.39133966, 0, -0.920246303)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+-- Teleport BF W2
+
+local Tab = Window:NewTab("Teleport Island (W2)")
+local Section = Tab:NewSection("Teleport BF V.3BB KAKKAK")
+Section:NewButton("Teleport Dressrosa", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-85.2854767, 107.393524, 337.12088, -0.664553225, -0, -0.747240901, -0, 1, -0, 0.747240901, 0, -0.664553225)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Cafe", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-378.350647, 73.0852814, 277.495514, -0.694111288, 0, 0.719867766, 0, 1, -0, -0.719867766, 0, -0.694111288)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+
+Section:NewButton("Teleport Green Biz", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2167.9917, 72.9877777, -2774.96021, -0.999236822, 0, 0.0390613005, 0, 1, -0, -0.0390613005, 0, -0.999236822)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Cold&Lab", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5973.02881, 15.9775639, -5109.18994, 0.0325278267, 8.29583868e-09, -0.99947083, -2.84207519e-10, 1, 8.29098123e-09, 0.99947083, 1.43695099e-11, 0.0325278267)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Raid", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-6438.90186, 255.544357, -4496.63184, -0.446822882, -0, -0.894622445, -0, 1, -0, 0.894622445, 0, -0.446822882)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Snow Mountain", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(500.450012, 401.447754, -5397.46729, 0.98791647, 0, -0.154987618, -0, 1, -0, 0.154987633, 0, 0.98791635)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Dark Arena", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3780.17236, 23.1181736, -3500.34033, -0.983933389, 0.0132311629, 0.17804502, 0.0305371527, 0.995026529, 0.0948140174, -0.175904989, 0.0987276733, -0.979443908)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Grave Yard", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5435.14844, 48.9511871, -791.8479, 0.445537031, -0.100292668, 0.889628053, 0.0261649489, 0.994739592, 0.0990387201, -0.89488107, -0.0208483599, 0.445817441)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Crused Ship", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(908.622742, 125.082909, 32900.1367, -0.99996841, 3.26972227e-08, -0.00794761442, 3.30787877e-08, 1, -4.78786433e-08, 0.00794761442, -4.81400271e-08, -0.99996841)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Ice Castle", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5461.56836, 28.2169418, -6265.82715, 0.585380554, 4.8196318e-09, -0.81075865, -1.25721114e-08, 1, -3.13266835e-09, 0.81075865, 1.20267512e-08, 0.585380554)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Forgotten", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3042.44458, 238.871994, -10215.0986, 0.998012066, 4.04258271e-09, -0.0630229712, -5.77653037e-09, 1, -2.73307332e-08, 0.0630229712, 2.7640457e-08, 0.998012066)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+-- Teleport BF W3
+
+local Tab = Window:NewTab("Teleport Island (W3)")
+local Section = Tab:NewSection("Teleport BF V.3BB KAKKAK")
+Section:NewButton("Teleport Port Town", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-277.793488, 6.75574446, 5331.92041, -0.944923043, 0, 0.327292562, 0, 1, 0, -0.327292562, 0, -0.944923043)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Hydra", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5237.77393, 1553.63538, -1460.58276, -0.999921441, -1.27410598e-08, 0.0125351166, -1.28769875e-08, 1, -1.07630296e-08, -0.0125351166, -1.0923598e-08, -0.999921441)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Great Tree", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2339.65942, 25.913353, -6515.70898, 0.480020493, -7.69285151e-08, -0.877257288, 5.36136753e-08, 1, -5.83555781e-08, 0.877257288, -1.90211136e-08, 0.480020493)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Castle", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5469.82324, 313.791321, -3210.05981, -0.989594638, 2.70828195e-08, 0.143883422, 2.14042082e-08, 1, -4.10146654e-08, -0.143883422, -3.75081832e-08, -0.989594638)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Floating Turtle", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-10000.249, 331.788452, -8336.08301, 0.0588201657, 4.34745795e-09, 0.998268604, 4.37029524e-09, 1, -4.61250549e-09, -0.998268604, 4.63403671e-09, 0.0588201657)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
+
+Section:NewButton("Teleport Mansion", "3BB KAKKAK", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-12549.7588, 337.194092, -7541.14404, 0.99643904, 4.38470238e-08, 0.0843160823, -3.91212183e-08, 1, -5.77009089e-08, -0.0843160823, 5.41968923e-08, 0.99643904)
+wait(0.1)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end)
